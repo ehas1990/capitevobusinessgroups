4539a602ac33405eb83356dad71546931dd33484
ehas1990 <73973579+ehas1990@users.noreply.github.com>
2022-06-04 09:40:53 +0530

    update mail 4-6-22 9:40AM

--- capitevo bank/mail_handler.php
+++ capitevo bank/mail_handler.php
@@ -1,20 +1,23 @@
 <?php
+error_reporting(E_ERROR | E_PARSE);
 	if(isset($_POST['submit'])){
 		$name=$_POST['name'];
 		$email=$_POST['email'];
 		$phone=$_POST['phone'];
 		$msg=$_POST['msg'];
-
-		$to='xyz.123@mail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Form Submission';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+    //   	$to='ehasalpha@gmail.com';
+		$to='capitevotvm@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Capitevo businessgroups';
+        $txt ="Name : ". $name . "\r\n email : " . $email . "\r\n  phone :" . $phone . "\r\n  msg :" . $msg;
 		$headers="From: ".$email;
 
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		if(mail($to,$subject,$txt,$headers)){
+			echo "<h1>Sent Successfully! Thank you ".$name.", We will contact you shortly!</h1>";
 		}
 		else{
 			echo "Something went wrong!";
 		}
 	}
+
+
 ?>
